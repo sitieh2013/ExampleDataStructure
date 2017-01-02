d28d29f3a987fb98d4ad9b7e17a2ae21aa8ed682
Yeiniel Alfonso Martínez <sitieh2013@gmail.com>
2017-01-01 20:58:00 -0500

    created the class NodeSimple and the ManagerNodeSimple

---
+++ ManagerNodeSimple.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuresExample
+{
+    public class ManagerNodeSimple<T> : ManagerNodeAbstract<T>
+    {
+        public override void AddNode(T item)
+        {
+            NodeAbstract<T> nodeNew = NodeSimple<T>.NewNode(item);
+            
+            if (root == null)
+                root = nodeNew;
+            else
+                finish.Next = nodeNew;
+
+            finish = nodeNew;
+            CountNode++;
+        }
+        
+        public override void InsertNode(int index, T item)
+        {
+            NodeAbstract<T> nodeNew = NodeSimple<T>.NewNode(item);
+
+            if (index == 0)
+            {   
+                nodeNew.Next = root;
+                root = nodeNew;
+            }
+            else
+            {
+                NodeAbstract<T> temp = FindNode(index - 1);
+                nodeNew.Next = temp.Next;
+                temp.Next = nodeNew;
+            }
+
+            CountNode++;
+        }
+    }
+}
---
+++ NodeSimple.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StructuresExample
+{
+    public class NodeSimple<T> : NodeAbstract<T>
+    { 
+        public static NodeAbstract<T> NewNode(T item)
+        {
+            NodeAbstract<T> Node = new NodeSimple<T>();
+            Node.Value = item;
+            return Node;
+        }
+    }
+}
