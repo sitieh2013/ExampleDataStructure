ac07e3901cf41ab67c0b194d262007f24309976c
Yeiniel Alfonso Martínez <sitieh2013@gmail.com>
2017-01-01 20:52:48 -0500

    Create abstract class

---
+++ NodeAbstract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuresExample
+{
+    public abstract class NodeAbstract<T>
+    {
+        public NodeAbstract<T> Next
+        {
+            get; set;
+        }
+
+        public T Value { get; set; }
+
+        public NodeAbstract(){}
+
+        public NodeAbstract(T item)
+        {
+            Value = item;
+        }
+        
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
