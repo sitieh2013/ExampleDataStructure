80aaa9afc670f89e43c28a7f1ef4eb16097dca7d
Yeiniel Alfonso Martínez <sitieh2013@gmail.com>
2017-01-01 20:58:17 -0500

    first example

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuresExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagerNodeSimple<int> manager = new ManagerNodeSimple<int>();
+            MyQueue<int> myQueue = new MyQueue<int>(manager);
+            
+            for(int i = 1; i < 10; i++)
+            {
+                myQueue.Enqueue(i);
+            }
+
+            Console.WriteLine(myQueue.Peek);
+
+            myQueue.Dequeue();
+
+            Console.WriteLine(myQueue.Peek);
+
+            Console.ReadLine();
+        }
+    }
+}
